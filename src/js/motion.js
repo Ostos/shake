82c8bbc450d11574f3ec8b032b27ea019cfdef4c
Gerardo Ostos <gog.ostos@gmail.com>
2015-12-08 23:49:53 -0600

    Added all files

--- src/js/motion.js
+++ src/js/motion.js
@@ -1,36 +1,35 @@
-var start,
-	delay = 3000;
-
-function deviceMotionHandler(e) {
+(function() {
+	var start,
+	delay = 3000,
+	state = 1,
+	isFinished = false,
+	body = document.getElementsByTagName("body")[0];
 	
-	if(!start) {
-		start = Date.now();
+	function changeState() {
+		if(state < 6){
+			body.classList.remove("state-" + state);
+			state++;
+			body.classList.add('state-' + state);
+		} else {
+			isFinished = true;
+		}
 	}
-	
-	if((Date.now() > start + delay) && 
-	((Math.abs(e.acceleration.x) + Math.abs(e.acceleration.y) +Math.abs(e.acceleration.z)) > 10)){			
-		var lis = document.getElementsByClassName("motion");
+
+	function deviceMotionHandler(e) {
 		
-		// acceleration is expressed in m/s^2
-		lis[0].innerHTML = e.acceleration.x;
-		lis[1].innerHTML = e.acceleration.y;
-		lis[2].innerHTML = e.acceleration.z;
-		lis[3].innerHTML = e.accelerationIncludingGravity.x;
-		lis[4].innerHTML = e.accelerationIncludingGravity.y;
-		lis[5].innerHTML = e.accelerationIncludingGravity.z;
-		// rotation rate expressed in deg/s
-		lis[6].innerHTML = e.rotationRate.alpha;
-		lis[7].innerHTML = e.rotationRate.beta;
-		lis[8].innerHTML = e.rotationRate.gamma;
-		lis[9].innerHTML = e.interval;
-		lis[10].innerHTML = Math.abs(e.acceleration.x);
-		lis[11].innerHTML = Math.abs(e.acceleration.y);
-		lis[12].innerHTML = Math.abs(e.acceleration.z);
-		lis[13].innerHTML = Math.abs(e.acceleration.x) + Math.abs(e.acceleration.y) +Math.abs(e.acceleration.z);
-		start = null;	
-	}
-}
+		if(isFinished === false) {
+			if(!start)
+				start = Date.now();
+		
+			if((Date.now() > start + delay) && ((Math.abs(e.acceleration.x) + Math.abs(e.acceleration.y) +Math.abs(e.acceleration.z)) > 10)){			
+				changeState();
+				start = null;	
+			}
+		}
 
-if (window.DeviceMotionEvent) {
-  window.addEventListener('devicemotion', deviceMotionHandler);
-} 
+	}
+	
+	if (window.DeviceMotionEvent)
+		window.addEventListener('devicemotion', deviceMotionHandler);
+	
+})();
